23e74e99733562c5384afa398033636e119f9232
Kazrek <mikololo26@interia.pl>
2022-08-15 05:21:23 +0200

    xdxdxdxdxd

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,13 +1,14 @@
 import axios from 'axios'
 
-let baseURL = ''
-if (process.env.NODE_ENV === 'development') {
-  baseURL = 'http://localhost:5000/api'
-} else if (process.env.NODE_ENV === 'production') {
-  baseURL = process.env.BASE_URL as string;
-} else {
-  baseURL = 'http://localhost:5000/api'
-}
+let baseURL = 'https://margostat-server.herokuapp.com/api'
+console.log(process.env.NODE_ENV)
+// if (process.env.NODE_ENV === 'development') {
+//   baseURL = 'http://localhost:5000/api'
+// } else if (process.env.NODE_ENV === 'production') {
+//   baseURL = process.env.BASE_URL as string;
+// } else {
+//   baseURL = 'http://localhost:5000/api'
+// }
 
 const instance = axios.create({
     baseURL: baseURL
