77fcd7302d870a2f373a25646b7a620feb16d958
Kazrek <mikololo26@interia.pl>
2022-08-15 05:03:09 +0200

    ZADZIALAJ, PROSZE! MATKO BOSKO!

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-let baseURL = '';
+let baseURL = ''
 if (process.env.NODE_ENV === 'development') {
   baseURL = 'http://localhost:5000/api'
 } else if (process.env.NODE_ENV === 'production') {
