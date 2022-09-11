8b252a5391cebd2d3b36e20ab9d3417b619fbe00
Kazrek <mikololo26@interia.pl>
2022-09-11 03:16:53 +0200

    nie mam pojecie co ja robie

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 let baseURL
 if (process.env.NODE_ENV === 'development') {
-    baseURL = 'http://localhost:5000/api'
+    baseURL = 'https://margostat-server.up.railway.app/api'
+    // baseURL = 'http://localhost:5000/api'
 } else if (process.env.NODE_ENV === 'production') {
     // baseURL = process.env.BASE_URL
     // baseURL = 'https://margostat-server.herokuapp.com/api'
